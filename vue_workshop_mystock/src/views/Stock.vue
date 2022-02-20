a440478e9767afba7f9968f46fd02e7b0eadc6a2
Portfolio-Premrutai <82368188+Portfolio-Premrutai@users.noreply.github.com>
2022-02-20 09:37:26 +0700

    02/20/ card

--- vue_workshop_mystock/src/views/Stock.vue
+++ vue_workshop_mystock/src/views/Stock.vue
@@ -1,13 +1,35 @@
 <template>
   <v-container>
-      <h1>Stock</h1>
+      <v-row>
+        <v-col lg="3" md="6" sm="12" col="12">
+           <StockCard title="TOTAL" subtitle="1,820" avatar_bg="#00a65a" avatar_ic="mdi-cart-outline" />
+        </v-col>
+         <v-col lg="3" md="6" sm="12" col="12">
+           <StockCard title="SOLD-OUT" subtitle="1,120" avatar_bg="#f39c12" avatar_ic="mdi-flask-outline" />
+        </v-col>
+        <v-col lg="3" md="6" sm="12" col="12">
+           <StockCard title="RETURN" subtitle="10" avatar_bg="#dd4b39" avatar_ic="mdi-keyboard-outline" />
+        </v-col>
+        <v-col lg="3" md="6" sm="12" col="12">
+           <StockCard title="DISCOUNT" subtitle="454" avatar_bg="#00c0ef" avatar_ic="mdi-cart-outline" />
+        </v-col>
+
+      </v-row>
   </v-container>
 </template>
 
 <script>
+import StockCard from "@/components/cards/StockCard";
 export default {
+name:"Stock",
+components:{
+StockCard
+},
+
+
 
 }
+
 </script>
 
 <style>
